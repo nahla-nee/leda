93dabfa7f07b254c422456f9d3523e62905e2b5c
Mustafa Hashim <zee.mzha@gmail.com>
2021-12-25 12:26:33 -0700

    pub use for error::Error

--- src/gemini/mod.rs
+++ src/gemini/mod.rs
@@ -1,10 +1,11 @@
 pub mod client;
-pub mod gemtext;
 pub mod header;
 pub mod response;
+pub mod gemtext;
 pub mod error;
 
 pub use client::Client;
 pub use header::Header;
 pub use response::Response;
 pub use gemtext::Gemtext;
+pub use error::Error;
