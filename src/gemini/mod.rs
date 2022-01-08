d57427e74754647d03a05187e77ec7101f18683e
zee-mzha <zee.mzha@gmail.com>
2022-01-07 20:17:14 -0700

    gemini module documentation

--- src/gemini/mod.rs
+++ src/gemini/mod.rs
@@ -1,7 +1,14 @@
+//! A module with all the gemini protocol functionality.
+
+/// Make gemini requests and parse their responses.
 pub mod client;
-pub mod header;
+/// Represent a gemini response.
 pub mod response;
+/// Represent a gemini response's header.
+pub mod header;
+/// Represent and parse gemtext documents.
 pub mod gemtext;
+/// The error type returned by functions in this module.
 pub mod error;
 
 pub use client::Client;
